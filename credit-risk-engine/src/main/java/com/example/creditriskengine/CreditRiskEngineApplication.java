9c844fad59f5cc369bec4c9b59627495aa9394c7
h-gipsiak <gipsiakhubert@gmail.com>
2026-02-17 15:35:30 +0100

    Project created

---
+++ credit-risk-engine/src/main/java/com/example/creditriskengine/CreditRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditriskengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRiskEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditRiskEngineApplication.class, args);
+    }
+
+}
