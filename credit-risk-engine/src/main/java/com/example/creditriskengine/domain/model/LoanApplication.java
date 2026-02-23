e262e96abaf5d555b81db930eeb31b507918f7bb
h-gipsiak <gipsiakhubert@gmail.com>
2026-02-23 15:47:49 +0100

    Added constructor for LoanApplication class

--- credit-risk-engine/src/main/java/com/example/creditriskengine/domain/model/LoanApplication.java
+++ credit-risk-engine/src/main/java/com/example/creditriskengine/domain/model/LoanApplication.java
@@ -4,12 +4,58 @@ import java.math.BigDecimal;
 
 public class LoanApplication {
 
-    private int age;
-    private BigDecimal monthlyIncome;
-    private EmploymentType employmentType;
-    private int creditHistoryYears;
-    private int existingLoansCount;
-    private int latePaymentsLastYear;
-    private BigDecimal requestedAmount;
-    
+    private final int age;
+    private final BigDecimal monthlyIncome;
+    private final EmploymentType employmentType;
+    private final int creditHistoryYears;
+    private final int existingLoansCount;
+    private final int latePaymentsLastYear;
+    private final BigDecimal requestedAmount;
+
+    public LoanApplication(int age,
+                           BigDecimal monthlyIncome,
+                           EmploymentType employmentType,
+                           int creditHistoryYears,
+                           int existingLoansCount,
+                           int latePaymentsLastYear,
+                           BigDecimal requestedAmount) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be at least 18");
+        }
+        this.age = age;
+        if (monthlyIncome == null) {
+            throw new IllegalArgumentException("MonthlyIncome must not be null");
+        } else if (monthlyIncome.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("MonthlyIncome must be greater than zero");
+        }
+        this.monthlyIncome = monthlyIncome;
+        if (employmentType == null) {
+            throw new IllegalArgumentException("EmploymentType must not be null");
+        }
+        this.employmentType = employmentType;
+        if (creditHistoryYears < 0) {
+            throw new IllegalArgumentException("CreditHistoryYears must not be negative");
+        } else if (creditHistoryYears > age - 18) {
+            throw new IllegalArgumentException("CreditHistoryYears must not be greater than age");
+        }
+        this.creditHistoryYears = creditHistoryYears;
+        if (existingLoansCount < 0) {
+            throw new IllegalArgumentException("ExistingLoans count must not be negative");
+        }
+        this.existingLoansCount = existingLoansCount;
+        if (latePaymentsLastYear < 0) {
+            throw new IllegalArgumentException("LatePaymentsLastYear must not be negative");
+        }
+        this.latePaymentsLastYear = latePaymentsLastYear;
+        if (requestedAmount == null) {
+            throw new IllegalArgumentException("RequestedAmount must not be null");
+        } else if (requestedAmount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("RequestedAmount must be greater than zero");
+        } else if (monthlyIncome.intValueExact()*12 > requestedAmount.intValueExact()) {
+            throw new IllegalArgumentException("RequestedAmount must not be higher than annual income");
+        }
+        this.requestedAmount = requestedAmount;
+    }
+
+
 }
